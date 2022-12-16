da0f0679f1d7eb3d29e8bad42d7365c6c2fbf3ac
Pavel <rut100500@yandex.ru>
2022-12-16 21:52:01 +0300

    через рекурсию вывести сумму чисел от M до N

---
+++ task66/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задайте значения M и N. Напишите метод, который найдёт сумму натуральных элементов в промежутке от M до N. Через рекурсию!
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+Console.WriteLine("Введите начало численного ряда число M:");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.WriteLine("Введите конец численного ряда число N:");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+if(!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибка ввода! ПереВВедите!");
+    return;
+}
+
+int sum = SumOfDigit(m, n);
+
+Console.WriteLine($"Сумма чисел = {sum}");
+
+int SumOfDigit(int m, int n)
+{
+    int sum = n;
+    if (n > m)
+    {
+        
+        sum += SumOfDigit(m, n - 1);
+        // Console.WriteLine(sum);
+    }
+    return sum;
+}
