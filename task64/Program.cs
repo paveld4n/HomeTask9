258a5f5efb6570b9b6dfb267c21e9a60158dabc0
Pavel <rut100500@yandex.ru>
2022-12-16 21:49:58 +0300

    через рекурсию вывести нат числа от N до 1

---
+++ task64/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задайте значение N. Напишите метод, который выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+Console.WriteLine("Введите число N:");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+if(!isParsedN || n <= 0)
+{
+    Console.WriteLine("Ошибка ввода! ПереВВедите!");
+    return;
+}
+
+PrintNumbers(n);
+
+void PrintNumbers(int x, int i = 1) // Значение I задается как значение по умолчанию. Соответственно его можно не передавать методом
+{
+    if(i > x)
+    {
+        return;
+    }
+    Console.Write($"{x} ");
+    PrintNumbers(x - 1);
+}
